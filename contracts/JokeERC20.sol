53bb35b6641dd81927d6672bd39079b78317915f
Makar Sokarev <makar.shokarev@askend.com>
2024-07-12 10:06:31 +0300

    Add dunction permit

--- contracts/JokeERC20.sol
+++ contracts/JokeERC20.sol
@@ -4,22 +4,30 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
-abstract contract JokeERC20 is Context, IERC20, IERC20Metadata {
+abstract contract JokeERC20 is Context, IERC20, IERC20Metadata, IERC20Permit {
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
-
-    uint256 private _totalTokens;
+    uint256 private _totalSupply;
 
     string private _name;
     string private _symbol;
-    uint8 private _decimals;
+    uint8 private _decimals = 18;
+
+    /// @notice The EIP-712 typehash for the contract's domain
+    bytes32 public constant DOMAIN_SEPARATOR = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+
+    /// @notice The EIP-712 typehash for the permit struct used by the contract
+    bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    /// @notice A record of states for signing / validating signatures
+    mapping (address => uint) public nonces;
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
-        _decimals = 18;
     }
 
     function name() external view override returns (string memory) {
@@ -35,7 +43,7 @@ abstract contract JokeERC20 is Context, IERC20, IERC20Metadata {
     }
     
     function totalSupply() external view returns (uint256) {
-        return _totalTokens;
+        return _totalSupply;
     }
 
     function balanceOf(address account) external view returns (uint256) {
@@ -70,13 +78,42 @@ abstract contract JokeERC20 is Context, IERC20, IERC20Metadata {
 
     function _mint(address account, uint256 value) internal virtual {
         _balances[account] += value;
-        _totalTokens += value;
+        _totalSupply += value;
         emit Transfer(address(0), account, value);
     }
 
     function _burn(address _from, uint256 value) internal virtual {
         _balances[_from] -= value;
-        _totalTokens -= value;
+        _totalSupply -= value;
     }
 
+    /**
+     * @notice Triggers an approval from owner to spends
+     * @param owner The address to approve from
+     * @param spender The address to be approved
+     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     * @param deadline The time at which to expire the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function permit(address owner, address spender, uint amount, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_SEPARATOR, keccak256(bytes(_name)), getChainId(), address(this)));
+        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, amount, nonces[owner]++, deadline));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "invalid signature");
+        require(signatory == owner, "unauthorized");
+        require(block.timestamp <= deadline, "signature expired");
+
+        _allowances[owner][spender] = amount;
+
+        emit Approval(owner, spender, amount);
+    }
+
+    function getChainId() internal view returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
 }
--- contracts/JokeToken.sol
+++ contracts/JokeToken.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 import "./JokeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract JokeToken is JokeERC20, Ownable  {
-    constructor(address initialOwner) JokeERC20("JokeToken", "JOKE") Ownable(initialOwner) { }
+contract JokeToken is JokeERC20, Ownable {
+    constructor(address initialOwner) JokeERC20("JokeToken", "JOKE") 
+        Ownable(initialOwner) 
+    { }
 
     function mint(address account, uint256 value) public onlyOwner {
         _mint(account, value);
